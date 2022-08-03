5213bdd68d2840382f3aea2a4ee4fb0899f5ef40
alex <agritsanov@list.ru>
2022-08-04 02:10:56 +0300

    WorkerServices(expiration)

---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,36 @@
+import { ExpirationCompleteEvent, Listener, OrderStatus, Subjects } from "@algreetickets/common";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
+import { natsWrapper } from "../../nats-wrapper";
+import { OrderCancelledPublisher } from "../order-cancelled-publisher";
+import { queueGroupName } from "./queue-group-name";
+
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+    queueGroupName = queueGroupName;
+    subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+
+    async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+        const order = await Order.findById(data.orderId).populate('ticket')
+
+        if (!order) {
+            throw new Error('Order not found')
+        }
+
+        order.set({
+            status: OrderStatus.Cancelled,
+
+        })
+        await order.save()
+
+        await new OrderCancelledPublisher(natsWrapper.client).publish({
+            id: order.id,
+            version: order.version,
+            ticket: {
+                id: order.ticket.id
+            }
+        })
+
+        msg.ack()
+    }
+}
