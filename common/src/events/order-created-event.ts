9f8d121532161b5779b2ea6d9fea9ffa95c5c9e0
alex <agritsanov@list.ru>
2022-08-07 05:03:49 +0300

    add common

---
+++ common/src/events/order-created-event.ts
@@ -0,0 +1,20 @@
+import { Subjects } from "./subjects";
+import { OrderStatus } from "./types/order-status";
+
+
+
+export interface OrderCreatedEvent {
+    subject: Subjects.OrderCreated;
+    data: {
+        id: string,
+        version: number;
+        status: OrderStatus,
+        userId: string;
+        expiresAt: string;
+        // date in json
+        ticket: {
+            id: string;
+            price: number
+        }
+    }
+}
---
+++ common/src/middlewares/error-handlers.ts
@@ -0,0 +1,43 @@
+import { Request, Response, NextFunction } from "express"
+import { CustomError } from "../errors/custom-error"
+import { DatabaseConnectionError } from "../errors/database-connection-error"
+import { RequestValidationError } from "../errors/request-validation-error"
+
+export const errorHandler = (
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction) => {
+    // console.log('Something went wrong', err)
+
+    // if (err instanceof RequestValidationError) {
+    //     return res.status(err.statusCode).send({ error: err.serializeErrors() })
+    //     // const formattedErrors = err.errors.map(error => {
+    //     //     return { message: error.msg, field: error.param }
+    //     // })
+    //     // return res.status(400).send({ errors: formattedErrors })
+    // }
+
+    // if (err instanceof DatabaseConnectionError) {
+    //     return res.status(err.statusCode).send({ error: err.serializeErrors() })
+    //     // return res.status(500).send({
+    //     //     errors: [
+    //     //         { message: err.reason }
+    //     //     ]
+    //     // })
+    // }
+
+    if (err instanceof CustomError) {
+        // console.log(err)
+        return res.status(err.statusCode).send({
+            errors: err.serializeErrors()
+        })
+    }
+
+    console.error(err)
+    res.status(400).send({
+        errors: [
+            { message: 'Something went wrong' }
+        ]
+    })
+}
